2aad9ed9732144ab2075c1e1f19ee857ff47d72e
Natalie Cook <natalie.t.cook@gmail.com>
2017-11-20 21:19:52 +0000

    check3

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,24 +1,26 @@
 require 'httparty'
+require 'json'
 
 class Kele
   
   include HTTParty
-  attr_accessor  :base_uri, :auth_token, :response
-  
- 
+  include JSON
+  attr_accessor  :base_uri, :auth_token
     
   def initialize(email, password)
     
     @base_uri = "https://www.bloc.io/api/v1"
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: password} )
-    
     @auth_token = response["auth_token"]
     
     unless @auth_token
       raise RuntimeError.new("Invalid credentials, please try again.")
     end
-      
   end    
+  
+  def get_me
+    response = JSON.parse(self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token }).body)
+  end  
   
 end    
     
